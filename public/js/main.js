7326de2f006e66f890a47d198640e805a7b885e9
simbavoyages <kigali@simbavoyages.com>
2025-08-14 15:18:41 +0200

    country code

--- public/js/main.js
+++ public/js/main.js
@@ -418,8 +418,8 @@ function displayResults(pnrResult, displayPnrOptions, fareDetails, baggageDetail
             const arrTerminalDisplay = flight.arrival.terminal ? ` (T${flight.arrival.terminal})` : '';
             const arrivalDateDisplay = flight.arrival.dateString ? ` on ${flight.arrival.dateString}` : '';
 
-            const departureString = `${flight.departure.airport}${depTerminalDisplay} - ${flight.departure.city}, ${flight.departure.name} at ${flight.departure.time}`;
-            const arrivalString = `${flight.arrival.airport}${arrTerminalDisplay} - ${flight.arrival.city}, ${flight.arrival.name} at ${flight.arrival.time}${arrivalDateDisplay}`;
+            const departureString = `${flight.departure.airport}${depTerminalDisplay} - ${flight.departure.city}(${flight.departure.countryCode}), ${flight.departure.name} at ${flight.departure.time}`;
+            const arrivalString = `${flight.arrival.airport}${arrTerminalDisplay} - ${flight.arrival.city}(${flight.arrival.countryCode}), ${flight.arrival.name} at ${flight.arrival.time}${arrivalDateDisplay}`;
 
             const detailRows = [
                 { label: 'Departing ', value: departureString },
